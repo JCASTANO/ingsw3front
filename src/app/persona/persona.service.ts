cec1f1cca949f267d243099f5b7cb5b794b3e4ad
Juan Esteban Castano Rincon <estebancastanorincon@gmail.com>
2022-04-05 17:48:53 -0500

    Ajuste de front primer consumo

--- src/app/persona/persona.service.ts
+++ src/app/persona/persona.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Persona } from './persona';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll() : Observable<Persona> {
+    return this.http.get<Persona>('http://localhost:8081/api/personas');
+  }
 }
